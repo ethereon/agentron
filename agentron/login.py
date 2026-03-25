7947cc46bb6108be6a67f96981dfa2d8dead4758
Saumitro Dasgupta <sd@cs.stanford.edu>
2026-03-25 12:30:00 -0700

    Propagate login script exit code

--- agentron/login.py
+++ agentron/login.py
@@ -1,11 +1,13 @@
 import subprocess
+import sys
 
 from agentron.rpc.flux import get_flux_path
 
 
 def main():
     flux_path = get_flux_path()
-    subprocess.run(['node', flux_path, 'login'])
+    result = subprocess.run(['node', flux_path, 'login'])
+    sys.exit(result.returncode)
 
 
 if __name__ == '__main__':
