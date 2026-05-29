292ea25c6061222dbd76d7b3e767c18b6a7bbba0
Saumitro Dasgupta <sd@cs.stanford.edu>
2026-05-29 11:21:29 -0700

    Refactor/simplify Publisher

--- agentron/utils/publisher.py
+++ agentron/utils/publisher.py
@@ -3,30 +3,35 @@ from typing import Callable
 
 type Subscriber[T] = Callable[[T], None]
 
-type Subscription = Callable[[], None]
 
+class Subscription[T]:
+    __slots__ = ('publisher', 'callback', 'active')
 
-class Entry[T]:
-    __slots__ = ('callback', 'index', 'active')
-
-    def __init__(self, callback: Subscriber[T], index: int):
+    def __init__(self, publisher: Publisher[T], callback: Subscriber[T]):
+        self.publisher: Publisher[T] | None = publisher
         self.callback = callback
-        self.index = index
         self.active = True
+
+    def __call__(self) -> None:
+        if self.publisher is None:
+            return
+
+        publisher, self.publisher = self.publisher, None
+        publisher._unsubscribe(self)
 
 
 class Publisher[T]:
     def __init__(self):
-        self._entries: list[Entry[T]] = []
+        self._subs: list[Subscription[T]] = []
         self._publishing = 0
         self._needs_compact = False
 
-    def subscribe(self, callback: Subscriber[T]) -> Subscription:
-        entry = Entry(callback, len(self._entries))
-        self._entries.append(entry)
-        return lambda: self._unsubscribe(entry)
+    def subscribe(self, callback: Subscriber[T]) -> Subscription[T]:
+        subscription = Subscription(self, callback)
+        self._subs.append(subscription)
+        return subscription
 
-    def publish(self, value: T):
+    def publish(self, value: T) -> None:
         """
         Publish a value to all active subscribers.
 
@@ -34,7 +39,7 @@ class Publisher[T]:
         However, subscriptions added during publishing won't be called until the next publish.
         """
         self._publishing += 1
-        end = len(self._entries)
+        end = len(self._subs)
 
         try:
             # Removals are deferred until after publishing finishes to avoid
@@ -42,75 +47,55 @@ class Publisher[T]:
             # New subscriptions during publishing are safe (added past iteration end)
             # but won't be called until the next publish.
             for i in range(end):
-                entry = self._entries[i]
-                if entry.active:
-                    entry.callback(value)
+                sub = self._subs[i]
+                if sub.active:
+                    sub.callback(value)
         finally:
             self._publishing -= 1
 
             if self._publishing == 0 and self._needs_compact:
-                self._compact()
+                # Prune inactive subscriptions
+                self._subs = [sub for sub in self._subs if sub.active]
                 self._needs_compact = False
 
-    def clear(self):
-        for entry in self._entries:
-            entry.active = False
+    def clear(self) -> None:
+        for sub in self._subs:
+            sub.active = False
+            sub.publisher = None
 
         if self._publishing:
             self._needs_compact = True
         else:
-            self._entries.clear()
+            self._subs.clear()
 
     @classmethod
-    def clear_all(cls, *publishers: Publisher):
+    def clear_all(cls, *publishers: Publisher) -> None:
         for publisher in publishers:
             publisher.clear()
 
-    def _unsubscribe(self, entry: Entry[T]):
-        if not entry.active:
+    def _unsubscribe(self, subscription: Subscription[T]) -> None:
+        if not subscription.active:
             # Will be pruned during deferred compaction.
             return
 
-        entry.active = False
+        subscription.active = False
 
         if self._publishing:
             # Defer removal until after publishing finishes to avoid mutating the list while iterating.
             self._needs_compact = True
         else:
             # Safe to eagerly remove.
-            self._remove(entry)
-
-    def _remove(self, entry: Entry[T]):
-        last = self._entries[-1]
-
-        if entry is last:
-            self._entries.pop()
-            return
-
-        # Swap with the last entry + pop for efficient removal.
-        self._entries[entry.index] = last
-        last.index = entry.index
-        self._entries.pop()
-
-    def _compact(self):
-        entries: list[Entry[T]] = []
-
-        for entry in self._entries:
-            if entry.active:
-                entry.index = len(entries)
-                entries.append(entry)
-
-        self._entries = entries
+            self._subs.remove(subscription)
 
 
 class SubscriptionStore:
     def __init__(self, *subs: Subscription):
         self.subscriptions = subs
 
-    def add(self, *subs: Subscription):
+    def add(self, *subs: Subscription) -> None:
         self.subscriptions += subs
 
-    def clear(self):
+    def clear(self) -> None:
         subs, self.subscriptions = self.subscriptions, ()
         for sub in subs:
             sub()
